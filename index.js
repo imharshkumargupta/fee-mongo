332a49f896185de9676d56cbadc03bae815f81a9
Harsh Kumar Gupta <imharshkumargupta@gmail.com>
2021-08-08 19:53:04 +0530

    added .gitignore, .env and changed the password of the mongo atlas server for security

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://admin:passwordpassword@cluster0.bm6he.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 mongoose.connection.on("connected", () => {
   console.log("Connected to the mongo server");
