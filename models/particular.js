e7ebf61609132a3abe00c69e4d3b6462d4cce3f1
Harsh Kumar Gupta <imharshkumargupta@gmail.com>
2021-08-08 20:12:49 +0530

    added particular schema

--- models/particular.js
+++ models/particular.js
@@ -1,5 +1,26 @@
 const mongoose = require("mongoose");
 
-const particularSchema = new mongoose.Schema({});
+const particularSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  class: {
+    type: String,
+    required: true,
+  },
+  month: {
+    type: String,
+    required: true,
+  },
+  session: {
+    type: String,
+    required: true,
+  },
+});
 
 mongoose.model("Particular", particularSchema);
