b50e36ff7a2efe85c0bd166a05e6411307fd1c91
Harsh Kumar Gupta <imharshkumargupta@gmail.com>
2021-08-08 19:26:49 +0530

    added student schema

--- models/student.js
+++ models/student.js
@@ -1,5 +1,24 @@
 const mongoose = require("mongoose");
 
-const studentSchema = new mongoose.Schema({});
+const studentSchema = new mongoose.Schema({
+  studentEmail: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  studentImage: {
+    type: String,
+  },
+  fatherName: {
+    type: String,
+  },
+  studentType: {
+    type: String,
+    enum: ["hostel", "transport"],
+  },
+});
 
 mongoose.model("Student", studentSchema);
