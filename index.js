f6755b3f44d5a25bd2467133db5b4260c5017711
Harsh Kumar Gupta <imharshkumargupta@gmail.com>
2021-08-20 16:34:58 +0530

    new code in index.js

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const transactionRoutes = require("./routes/transaction");
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(adminRoutes);
 app.use(studentRoutes);
 app.use(particularRoutes);
